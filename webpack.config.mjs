78cbcdc50b27054aa24686d3261f3c1dbd036264
tomrosser100 <tomrosser100@gmail.com>
2024-07-28 14:30:50 +0100

    rm dist

--- webpack.config.mjs
+++ webpack.config.mjs
@@ -8,7 +8,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const mode = "development";
 
 const frontConfig = {
-  devtool: "source-map",
+  devtool: "inline-source-map",
   devServer: {
     static: {
       directory: "./front/dist",
